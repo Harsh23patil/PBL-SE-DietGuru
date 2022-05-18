9d22f4990dc8e0550085a0bd66eb7c685b7ee28a
Harshal Patil <86585860+Harsh23patil@users.noreply.github.com>
2022-05-18 17:48:46 +0530

    Adding colories Function

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ button.addEventListener('click', () => {
     const height = parseInt(document.getElementById('height').value);
     const weight = parseInt(document.getElementById('weight').value);
     const result = document.getElementById('output');
+    const result2 = document.getElementById('outputcalri');
+    const age = parseFloat(document.getElementById('Age').value);
     let height_status=false, weight_status=false;
 
     if(height === '' || isNaN(height) || (height <= 0)){
@@ -20,11 +22,13 @@ button.addEventListener('click', () => {
 
     if(height_status && weight_status){
         const bmi = (weight / ((height*height)/10000)).toFixed(2);
-
+        const calori = ((weight * 10 + 6 * height - ((5 * age)+ 5) ) * 1.7).toFixed(2);
+        result2.innerHTML = 'calorie needed per day : ' + calori; 
         if(bmi < 18.6){
             result.innerHTML = 'Under weight : ' + bmi;
         }else if(bmi >= 18.6 && bmi < 24.9){
             result.innerHTML = 'Normal : ' + bmi;
+            
         }else{
             result.innerHTML = 'Over weight : ' + bmi;
         }
